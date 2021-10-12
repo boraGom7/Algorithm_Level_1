0fa94a34650f6b4f03eb5eb977240a22072c9aa2
imhyejeong <83940751+boraGom7@users.noreply.github.com>
2021-10-12 18:18:00 +0900

    문자열 다루기 기본

--- 문자열 다루기 기본.playground/Contents.swift
+++ 문자열 다루기 기본.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-let s = "a234"
+let s = "12j6"
+print(s.count)
 
-print(s.allSatisfy({ $0.isNumber }))
-
+print(s.count == 4 || s.count == 6 ? s.allSatisfy({ $0.isNumber}) : false)
 /*
  allSatisfy: 해당하는 값이 모두 조건을 만족하면 true, 아니면 false
  */
